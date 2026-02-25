bd256fb859a4586abec82d26a61470b17e1c1188
alison <adevpr11@gmail.com>
2026-02-25 10:05:24 -0300

    chore: estrutura inicial

---
+++ src/test/java/com/nca/usuariosapi/UsuariosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nca.usuariosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
